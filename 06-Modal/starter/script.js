d68fbf5398cf3ef5b34958cb6b0d434bb040bf26
Kaique Xavier <kaiquexatalla@gmail.com>
2026-07-28 19:22:58 -0300

    criando o projeto 2 - apresentar modal
    Nesse projeto foi utilizado o conceito de ouvir eventos do teclado, além
    de criar funções para adicionar ou remover classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn_close_modal = document.querySelector('.close-modal');
+const btns_open_modal = document.querySelectorAll('.show-modal');
+
+const open_modal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+const close_modal = function () {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+
+}
+
+
+for (let i = 0; i < btns_open_modal.length; i++) {
+    // console.log(btns_open_modal[i].textContent);
+    btns_open_modal[i].addEventListener('click', open_modal)
+}
+
+btn_close_modal.addEventListener('click', close_modal)
+overlay.addEventListener('click', close_modal)
+
+document.addEventListener('keydown', function (e) {
+    // e torna-se o objeto do evento. E é possivel acessar os atributos com 
+    // o "."
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        close_modal();
+    }
+})
